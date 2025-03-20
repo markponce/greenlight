533e576595e4443b9cdce03097a7765995c06f38
Mark Anthony Ponce <markponce07@gmail.com>
2025-03-21 00:06:01 +0800

    Add writeJSON and update readIDParam to use base 10 and bitsize to 64

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -8,12 +9,42 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) readIDParam(r *http.Request) (int, error) {
+func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 
-	id, err := strconv.Atoi(params.ByName("id"))
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
 	if err != nil || id < 1 {
 		return 0, errors.New("invalid id parameter")
 	}
+
 	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	// convert to json
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// write new line for command line viewing
+	js = append(js, '\n')
+
+	// // Add headers to reponse writer header
+	// maps.Insert(w.Header(), maps.All(headers))
+
+	// Add headers to reponse writer header
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	// add standard header
+	w.Header().Set("Content-Type", "application/json")
+	// add status code
+	w.WriteHeader(status)
+	// write json to writer
+	w.Write(js)
+
+	// no error
+	return nil
 }
