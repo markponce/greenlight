32bbc1cb3daccb015b613c723763e66ae4606cfd
Mark Anthony Ponce <markponce07@gmail.com>
2025-03-20 16:31:35 +0800

    Create cmd/api/main.go

---
+++ cmd/api/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+// Declare a string containing the application version number.
+const version = "1.0.0"
+
+// Define a config struct to hold all the configuration settings for our application.
+type config struct {
+	port int
+	env  string
+}
+
+// Define an application struct to hold the dependencies for our HTTP handlers, helpers,
+// and middleware.
+type application struct {
+	config config
+	logger *slog.Logger
+}
+
+func main() {
+	// Declare an instance of the config struct.
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (develop|stagin|production)")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := application{
+		config: cfg,
+		logger: logger,
+	}
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", app.config.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	logger.Info("Starting server", "addr", srv.Addr, "env", cfg.env)
+
+	err := srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
+
+}
