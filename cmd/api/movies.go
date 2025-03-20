efd96272bd7f2447ee7e9290347f87f5a242f47a
Mark Anthony Ponce <markponce07@gmail.com>
2025-03-21 00:00:26 +0800

    return a single movie

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/markponce/greenlight/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,11 +14,25 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 
-	movieID, err := app.readIDParam(r)
+	id, err := app.readIDParam(r)
 	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of movie %d\n", movieID)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablance",
+		Year:      2001,
+		RunTime:   102,
+		Genres:    []string{},
+		Vesion:    1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "he server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
