95fedd8c37468211293e52ff3423164174c348e5
Mark Anthony Ponce <markponce07@gmail.com>
2025-05-02 00:08:36 +0800

    Add user permissions

---
+++ internal/data/permissions.go
@@ -0,0 +1,73 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"slices"
+	"time"
+
+	"github.com/lib/pq"
+)
+
+type Permissions []string
+
+func (p Permissions) Include(code string) bool {
+	return slices.Contains(p, code)
+}
+
+type PermissionModel struct {
+	DB *sql.DB
+}
+
+func (m PermissionModel) GetAllForUser(userId int64) (Permissions, error) {
+
+	query := `SELECT permissions.code
+        FROM permissions
+        INNER JOIN users_permissions ON users_permissions.permission_id = permissions.id
+        INNER JOIN users ON users_permissions.user_id = users.id
+        WHERE users.id = $1`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	rows, err := m.DB.QueryContext(ctx, query, userId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var permissions Permissions
+
+	for rows.Next() {
+		var permission string
+
+		err := rows.Scan(&permission)
+		if err != nil {
+			return nil, err
+		}
+
+		permissions = append(permissions, permission)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return permissions, nil
+}
+
+func (m PermissionModel) AddForUser(userId int64, code ...string) error {
+	query := `
+		insert into users_permissions 
+		select $1, permissions.id from permissions where permissions.code = ANY($2)
+	`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	_, err := m.DB.ExecContext(ctx, query, userId, pq.Array(code))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
